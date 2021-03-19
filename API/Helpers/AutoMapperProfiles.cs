971d8d9f3dafbc67310f2096100fa01a4acbaf82
LukaszPan <lukaszpanczykowski@gmail.com>
2021-03-19 23:51:48 +0100

    AutoMapper + Extending User Enitity + Seed data

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using API.DTO;
+using API.Entites;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser,MemberDto>()
+            .ForMember(destinationMember => destinationMember.PhotoUrl,
+                memberOptions=>memberOptions.MapFrom(src => src.Photos.FirstOrDefault(x=>x.IsMain).Url))
+            .ForMember(dest => dest.Age, otp  => otp.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo,PhotoDto>();
+        }
+    }
+}
